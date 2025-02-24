e771bbc3b3fef0611ee080f09901eb46266f0f6b
imihirjain <jmanav251@gmail.com>
2025-02-25 01:25:47 +0530

    Complete Editor store, Editor tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,17 +2,86 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from "tiptap-extension-resize-image"
+import { useEditorStore } from '@/store/use-editor-store';
+import Underline from '@tiptap/extension-underline'
 export const Editor = ()=>{
+  const {setEditor}= useEditorStore()
     const editor = useEditor({
+       onCreate({editor}){
+        setEditor(editor)
+       },
+       onDestroy()
+       {
+        setEditor(null)
+       },
+       onUpdate({editor})
+       {
+        setEditor(editor)
+       },
+       onSelectionUpdate({editor})
+       {
+        setEditor(editor)
+       },
+       onTransaction({editor})
+       {
+        setEditor(editor)
+       },
+       onFocus({editor})
+       {
+        setEditor(editor)
+       },
+       onBlur({editor})
+       {
+        setEditor(editor)
+       },
+       onContentError({editor})
+       {
+        setEditor(editor)
+       },
         editorProps:{
             attributes:{
                 style:"padding-left:56px; padding-right:56px;",
                 class:"focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             },
         },
-        extensions: [StarterKit],
-        content: '<p>Hello World! 🌎️</p>',
+        extensions: [
+            StarterKit,
+            TaskList,
+            Underline,
+            Table,
+            TableCell,
+            TableHeader,
+            ImageResize,
+            TableRow,
+            Image,
+            TaskItem.configure({
+                nested: true,
+            }),
+        ],
+        content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
       })
     return(
         <div className='size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible'>
@@ -22,3 +91,4 @@ export const Editor = ()=>{
         </div>
     )
 }
+
