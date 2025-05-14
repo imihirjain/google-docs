f05eff04698827632bcf8cdbdd8d8ac6d1bb4a38
manavjain1815 <manav@webatdoor.com>
2025-05-14 19:31:54 +0530

    final change

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -36,12 +36,24 @@ export async function POST(req:Request){
         return new Response("Unauthorized",{status:401})
     }
 
+//     const userEmail = user.primaryEmailAddress?.emailAddress || user.emailAddresses?.[0]?.emailAddress;
+// const userName = user.fullName || userEmail || "Anonymous";
+
+const name =user.fullName??user.primaryEmailAddress?.emailAddress??"Anonymous";
+
+const nameToNumber = name.split("").reduce((acc,char)=>acc+char.charCodeAt(0),0);
+
+const hue = Math.abs(nameToNumber)%360;
+const color = `hsl(${hue},80%,60%)`;
+
     const session = liveblocks.prepareSession(user.id,{
         userInfo:{
-            name:user.fullName?? user.primaryEmailAddress?.emailAddress??"Anonymous",
+            name,
             avatar:user.imageUrl,
+            color,
         }
     })
+    console.log(user)
     session.allow(room,session.FULL_ACCESS);
     const {body,status}=await session.authorize();
     return new Response(body,{status})
